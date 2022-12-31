d06cdb80c0063b3b03ae9e859b0bdff12537f158
Ofir Arbili <arbili@Ofirs-MBP-2.lan>
2022-12-31 11:56:14 +0200

    bucket test

--- search_frontend.py
+++ search_frontend.py
@@ -10,9 +10,9 @@ from nltk.corpus import stopwords
 import json
 nltk.download('stopwords')
 
-# client = storage.Client()
-# bucket = client.get_bucket('bx_title_index')
-# idx_title = pickle.loads(bucket.get_blob('postings_gcp/bx_title_index_index.pkl').download_as_string())
+client = storage.Client()
+bucket = client.get_bucket('bx_title_index')
+idx_title = pickle.loads(bucket.get_blob('postings_gcp/bx_title_index_index.pkl').download_as_string())
 
 # bucket = client.get_bucket('bx_body_index')
 # idx_body = pickle.loads(bucket.get_blob('postings_gcp/bx_body_index_index.pkl').download_as_string())
